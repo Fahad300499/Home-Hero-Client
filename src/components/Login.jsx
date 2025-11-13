052f0e13c86455342ea431af41de465cb4deeb55
Fahad300499 <fahad.147448@gmail.com>
2025-11-13 13:03:32 +0600

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -72,7 +72,7 @@ const Login = () => {
                             <label className="label">Password</label>
                             <input type="password" name='password' className="input" placeholder="Password" />
 
-                            {/* **** */}
+                            <div><a className="link link-hover">Forgot password?</a></div>
                             <button className="btn btn-neutral mt-4">Login</button>
                         </fieldset>
                     </form>
