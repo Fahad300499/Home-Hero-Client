e8b0ad5a6e91f11a3355129f736aa42f53442298
Fahad300499 <fahad.147448@gmail.com>
2025-11-13 13:00:40 +0600

    error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router';
 const Error = () => {
     return (
         <div className='text-center mt-50'>
-            {/* **** */}
+            <h2 className='text-2xl'>Error </h2>
+            <p className='text-8xl font-bold mb-10'>404</p>
+            <Link to='/' className='btn btn-primary'>Back To Home</Link>
         </div>
     );
 };
