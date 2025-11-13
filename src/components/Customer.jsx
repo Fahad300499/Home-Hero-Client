2fb41d0d922592969eb33e1e352396f653066204
Fahad300499 <fahad.147448@gmail.com>
2025-11-13 13:00:03 +0600

    Customer

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -39,7 +39,21 @@ const Customer = () => {
             </div>
 
 
-           {/* ****** */}
+           <div class="bg-white p-8 rounded-xl shadow-xl transition hover:shadow-2xl">
+                <div class="flex items-center text-yellow-500 mb-4">
+                    ⭐⭐⭐⭐
+                </div>
+                <p class="text-gray-700 italic text-lg mb-6">
+                    "I never thought I could book a service so quickly and easily. The whole process was incredibly smooth and the support team was extremely helpful."
+                </p>
+                <div class="flex items-center">
+
+                    <div>
+                        <p class="font-bold text-indigo-700">Rasel Farhad</p>
+                    </div>
+                </div>
+                <p class="text-xs mt-3 text-indigo-400">Book Service: Package 1</p>
+            </div>
             </div>
 
 
