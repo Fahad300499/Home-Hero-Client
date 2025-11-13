b71c9db047ed8f8f4f6a03fcc81adbb3086cd52f
Fahad300499 <fahad.147448@gmail.com>
2025-11-13 13:07:02 +0600

    profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -167,7 +167,13 @@ const Profile = () => {
                         </div>
 
                         <div className='flex justify-end space-x-3 pt-2'>
-                            {/* ******* */}
+                           <button 
+                                type="button" 
+                                onClick={handleCancel}
+                                className='px-4 py-2 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400'
+                            >
+                                Cancel
+                            </button>
                             <button 
                                 type="submit" 
                                 className='px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 font-semibold'
